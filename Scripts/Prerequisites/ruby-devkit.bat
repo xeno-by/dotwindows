a62ae574a00930dd3bab563194bcd6d84ef09970
xeno_by <xeno.by@gmail.com>
2011-11-17 10:25:33 +0100

    Script runtimes: ruby

---
+++ Scripts/Prerequisites/ruby-devkit.bat
@@ -0,0 +1,9 @@
+@echo off
+
+unzip "%~dp0\ruby-devkit.zip" -d "%TMP%"
+deploy-copy "%TMP%\ruby-devkit" "%HOMEDRIVE%\RubyDevKit451"
+rmdir "%TMP%\ruby-devkit" /S /Q
+
+cd /D "%HOMEDRIVE%\RubyDevKit451"
+ruby dk.rb init
+ruby dk.rb install
