58c518dd911b7f176ec504db74c095a4900a9f58
xeno_by <xeno.by@gmail.com>
2011-11-21 23:14:08 +0100

    myke-kepler test

--- Scripts/Scripts/myke-kep.cs
+++ Scripts/Scripts/myke-kep.cs
@@ -1,10 +1,14 @@
-// build this with "csc /t:exe /out:myke.exe /debug+ myke*.cs"
+// build this with "csc /t:exe /out:myke.exe /debug+ /r:System.Xml.Linq.dll myke*.cs"
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 [Connector(name = "kep", description =
   "Wraps the development workflow of project Kepler.\r\n" +
@@ -41,8 +45,20 @@ public class Kep : Git {
 
   [Action]
   public virtual ExitCode test() {
-    Console.println("error: not yet implemented");
-    Console.println("as far as I can see, it should invoke Kepler-related tests via partest");
-    return -1;
+    var project = new FileInfo(root + "\\.project");
+    if (!project.Exists) {
+      println("error: project file not found");
+      return -1;
+    }
+
+    var doc = XDocument.Load(@"C:\Projects\Kepler\.project");
+    if (doc == null) {
+      println("error: project file has unexpected format");
+      return -1;
+    }
+
+    var tests = ((IEnumerable<Object>)doc.XPathEvaluate("//linkedResources//locationURI[starts-with(text(), 'PROJECT_LOC/test/files') and substring(text(), string-length(text()) - 5) = '.scala']/text()")).Cast<XText>().Select(text => text.Value).ToList();
+    var home = new DirectoryInfo(root.FullName + "\\test");
+    return Console.batch("partest " + String.Join(" ", tests.Select(test => test.Substring(17))), home: home);
   }
 }
