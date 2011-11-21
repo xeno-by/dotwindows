efcd811f4795d46968d5ce56c7979d0fa6c30689
xeno_by <xeno.by@gmail.com>
2011-11-21 21:49:26 +0100

    yay! myke-sbt now also supports run! DONE

--- Scripts/Scripts/myke-sbt.cs
+++ Scripts/Scripts/myke-sbt.cs
@@ -17,12 +17,12 @@ public class Sbt : Prj {
 
   public virtual String sbtproject { get { return null; } }
 
-  public DirectoryInfo sbtRoot { get {
+  public DirectoryInfo sbtroot { get {
     // todo. do we need to cache this?
-    return detectSbtRoot();
+    return detectsbtroot();
   } }
 
-  public virtual DirectoryInfo detectSbtRoot() {
+  public virtual DirectoryInfo detectsbtroot() {
     var wannabe = file != null ? file.Directory : dir;
     while (wannabe != null) {
       var buildSbt = wannabe.GetFiles().FirstOrDefault(child => child.Name == "build.sbt");
@@ -35,72 +35,116 @@ public class Sbt : Prj {
   }
 
   public override bool accept() {
-    return base.accept() && dir.EquivalentTo(sbtRoot);
+    return base.accept() && dir.EquivalentTo(sbtroot);
   }
 
   [Action]
   public virtual ExitCode rebuild() {
     var preamble = String.Format("sbt {0}", sbtproject == null ? null : "\"project " + sbtproject + "\"");
-    return Console.batch(String.Format("{0} clean compile", preamble), home: sbtRoot);
+    return Console.batch(String.Format("{0} clean compile", preamble), home: sbtroot);
   }
 
   [Default, Action]
   public virtual ExitCode compile() {
     var preamble = String.Format("sbt {0}", sbtproject == null ? null : "\"project " + sbtproject + "\"");
-    return Console.batch(String.Format("{0} compile", preamble), home: sbtRoot);
+    return Console.batch(String.Format("{0} compile", preamble), home: sbtroot);
   }
 
-  [Action]
-  public virtual ExitCode repl() {
-    var log = new FileInfo("%TMP%\\sbt.log".Expand());
+  private class ProjectInfo {
+    public String scalahome;
+    public List<String> classpath;
+    public List<String> mainclasses;
+  }
+
+  private ProjectInfo compileAndInfer() {
+    var log = new FileInfo(Path.GetTempFileName());
     if (log.Exists) log.Delete();
 
     var preamble = String.Format("sbt {0}", sbtproject == null ? null : "\"project " + sbtproject + "\"");
-    Console.batch(String.Format("{0} compile \"show runtime:scala-home\" \"show runtime:dependency-classpath\" | tee {1}", preamble, log.FullName), home: sbtRoot);
+    Console.batch(String.Format("{0} compile \"show runtime:scala-home\" \"show runtime:dependency-classpath\" \"show discovered-main-classes\" | tee {1}", preamble, log.FullName), home: sbtroot);
 
-    if (!log.Exists) return -1;
+    if (!log.Exists) return null;
     var lines = File.ReadAllLines(log.FullName);
-    if (lines.Any(line => line.StartsWith("[error]"))) return -1;
+    if (lines.Any(line => line.StartsWith("[error]"))) return null;
     Console.println();
 
-    var s_scalahome = lines[lines.Count() - 3];
+    var s_scalahome = lines[lines.Count() - 5];
     var r_scalahome = new Regex(@"^\[info\] Some\((?<path>.*)\)$");
     var m_scalahome = r_scalahome.Match(s_scalahome);
-    if (!m_scalahome.Success) return -1;
+    if (!m_scalahome.Success) return null;
     var scalahome = m_scalahome.Result("${path}");
     if (Config.verbose) { Console.println("scalahome = {0}", scalahome); }
 
-    var s_classpath = lines[lines.Count() - 2];
+    var s_classpath = lines[lines.Count() - 4];
     var r_classpath = new Regex(@"^\[info\] ArrayBuffer\((?<paths>.*)\)$");
     var m_classpath = r_classpath.Match(s_classpath);
-    if (!m_classpath.Success) return -1;
+    if (!m_classpath.Success) return null;
     s_classpath = m_classpath.Result("${paths}");
     r_classpath = new Regex(@"Attributed\((?<path>.*?)\)");
     m_classpath = r_classpath.Match(s_classpath);
-    if (!m_classpath.Success) return -1;
+    if (!m_classpath.Success) return null;
     var classpath = new List<String>();
     for (; m_classpath.Success; m_classpath = m_classpath.NextMatch())
       classpath.Add(m_classpath.Result("${path}"));
     if (Config.verbose) { Console.println("classpath = {0}", String.Join("; ", classpath)); }
 
-    var scala = (scalahome + "\\bin\\scala.bat").GetShortPath();
+    var s_mainclasses = lines[lines.Count() - 2];
+    var r_mainclasses = new Regex(@"^\[info\] List\((?<classes>.*)\)$");
+    var m_mainclasses = r_mainclasses.Match(s_mainclasses);
+    if (!m_mainclasses.Success) return null;
+    s_mainclasses = m_mainclasses.Result("${classes}");
+    var mainclasses = s_mainclasses.Split(',').Select(mainclass => mainclass.Trim()).ToList();
+    if (Config.verbose) { Console.println("mainclasses = {0}", String.Join(", ", mainclasses)); }
+
+    if (Config.verbose) { Console.println(); }
+    return new ProjectInfo{scalahome = scalahome, classpath = classpath, mainclasses = mainclasses};
+  }
+
+  [Action]
+  public virtual ExitCode repl() {
+    var info = compileAndInfer();
+    if (info == null) return -1;
+
+    var scala = (info.scalahome + "\\bin\\scala.bat").GetShortPath();
     var options = new List<String>();
     options.Add("-deprecation");
-    options.Add("-classpath " + String.Join(";", classpath.Select(path => path.GetShortPath())));
+    options.Add("-classpath " + String.Join(";", info.classpath.Select(path => path.GetShortPath())));
     return Console.interactive(scala + " " + String.Join(" ", options.ToArray()));
   }
 
   [Action]
   public virtual ExitCode run(Arguments arguments) {
-    Console.println("error: not yet implemented");
-    Console.println("this should extract classpath and scalahome information from an sbt project");
-    Console.println("and run a standalone app, since sbt isn't good at being integrated");
-    return -1;
+    var info = compileAndInfer();
+    if (info == null) return -1;
+
+    String mainclass = null;
+    if (info.mainclasses.Count() == 0) {
+      Console.println("error: no main classes have been detected");
+      return -1;
+    } else if (info.mainclasses.Count() == 1) {
+      mainclass = info.mainclasses[0];
+    } else {
+      Console.println("Please, select one of the detected mainclasses: {0} or {1}", String.Join(", ", info.mainclasses.Take(info.mainclasses.Count() - 1)), info.mainclasses[info.mainclasses.Count() - 1]);
+      Func<String> readMainclass = () => Console.readln(prompt: "Mainclass", history: String.Format("mainclass {0}", sbtroot.FullName));
+      mainclass = readMainclass();
+      if (mainclass == String.Empty) {
+        Console.println("error: mainclass cannot be empty");
+        return -1;
+      }
+    }
+
+    var scala = (info.scalahome + "\\bin\\scala.bat").GetShortPath();
+    var options = new List<String>();
+    options.Add("-classpath " + String.Join(";", info.classpath.Select(path => path.GetShortPath())));
+    options.Add(mainclass);
+    Func<String> readArguments = () => Console.readln(prompt: "Run arguments", history: String.Format("run {0}", sbtroot.FullName));
+    options.Add(arguments.Count > 0 ? arguments.ToString() : readArguments());
+    return Console.interactive(scala + " " + String.Join(" ", options.ToArray()));
   }
 
   [Action]
   public virtual ExitCode test() {
     var preamble = String.Format("sbt {0}", sbtproject == null ? null : "\"project " + sbtproject + "\"");
-    return Console.batch(String.Format("{0} test", preamble), home: sbtRoot);
+    return Console.batch(String.Format("{0} test", preamble), home: sbtroot);
   }
 }
