650af59b09b03384c7516150f7a7a49cf52746ac
Eugene Burmako <xeno.by@gmail.com>
2011-12-09 14:49:23 +0100

    fix myke.el

--- Scripts/Scripts/myke-frontend.el
+++ Scripts/Scripts/myke-frontend.el
@@ -1,32 +1,32 @@
 (defun my-compile-project (buffer &optional callback)
-  (myke-invoke "compile" buffer &optional callback))
+  (myke-invoke "compile" buffer callback))
 
 (defun my-clean-project (buffer &optional callback)
-  (myke-invoke "clean" buffer &optional callback))
+  (myke-invoke "clean" buffer callback))
 
 (defun my-rebuild-project (buffer &optional callback)
-  (myke-invoke "rebuild" buffer &optional callback))
+  (myke-invoke "rebuild" buffer callback))
 
 (defun my-run-project (buffer &optional callback)
-  (myke-invoke "run" buffer &optional callback))
+  (myke-invoke "run" buffer callback))
 
 (defun my-repl-project (buffer &optional callback)
-  (myke-invoke "repl" buffer &optional callback))
+  (myke-invoke "repl" buffer callback))
 
 (defun my-run-test-project (buffer &optional callback)
-  (myke-invoke "run-test" buffer &optional callback))
+  (myke-invoke "run-test" buffer callback))
 
 (defun my-commit-project (buffer &optional callback)
-  (myke-invoke "commit" buffer &optional callback))
+  (myke-invoke "commit" buffer callback))
 
 (defun my-logall-project (buffer &optional callback)
-  (myke-invoke "logall" buffer &optional callback))
+  (myke-invoke "logall" buffer callback))
 
 (defun my-logthis-project (buffer &optional callback)
-  (myke-invoke "logthis" buffer &optional callback))
+  (myke-invoke "logthis" buffer callback))
 
 (defun my-pull-project (buffer &optional callback)
-  (myke-invoke "pull" buffer &optional callback))
+  (myke-invoke "pull" buffer callback))
 
 (defun my-push-project (buffer &optional callback)
-  (myke-invoke "push" buffer &optional callback))
+  (myke-invoke "push" buffer callback))
